88655621ef5635006da51deaf1f74189826b2737
Ducxy <59884498+MrDucxy@users.noreply.github.com>
2021-06-05 04:12:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const botConfig = require("./config/bot.json"); //Fetch the bot's config file.
 client.commands = new discord.Collection(); //Store all bot commands.
 client.aliases = new discord.Collection(); //Store all bot command aliases.
 client.events = new discord.Collection(); //Stores all bot events.
-client.queue = new Map(); //Server queues for music commands.
 
 ['command', 'event'].forEach((handler) => {
     require(`./handlers/${handler}`)(client);
